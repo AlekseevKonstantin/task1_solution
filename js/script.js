1e8aeb5b06ee7e8558945a2e6c36ea51143d1333
Alexeev Konstantin <myworkmailbox@list.ru>
2019-01-11 18:44:23 +0500

    release 0.0.1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
-  $('.preview-btn').on('click', function(e){
-    e.preventDefault();
+  function parseField(value){
+    if(!/^[+-_]?\d+$/gi.test(value)){
+        return value.replace(/[a-zA-Z]+/gi,'');
+    }
+  }
 
-    $(this).fadeOut(500);
-    setTimeout(function(){
-      $('.bootstrap-carusel').fadeIn(500);
-    }, 5000);
-    
-  })
-})
+  $('#phoneNum').keyup(function(){
+    $(this).val(parseField($(this).val()));
+  });
+  
+});
